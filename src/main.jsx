3278b15afb68cd3b86c35cafe50ca5e8221c21dd
Cesar Garzón <156729288+CesarG-09@users.noreply.github.com>
2026-05-01 00:28:39 +0000

    Integración Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
+import { Analytics } from '@vercel/analytics/react';
 
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
-import { Analytics } from "@vercel/analytics/react"
 
 import './styles/global.css';
 
