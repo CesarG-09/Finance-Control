9670b785ecc5b02cb9b380b7d7ee171ba200b222
Cesar Garzón <156729288+CesarG-09@users.noreply.github.com>
2026-05-01 00:22:00 +0000

    Integración de Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Analytics />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
